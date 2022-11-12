81a8c59f3ee77adb5ce71bcd5522016b241369b4
Connor Doman <domanc@student.ubc.ca>
2022-11-11 21:53:47 -0800

    reinstall node_modules

--- routes/listorder.js
+++ routes/listorder.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const sql = require("mssql");
+const sql = require("mysql");
 const moment = require("moment");
 const sv = require("../server");
 
